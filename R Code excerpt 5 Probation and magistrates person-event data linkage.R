92cd4254d49302ead76557541a8de61a1bceb844
CarlyLL <carly.lightowlers@gmail.com>
2024-06-07 08:55:09 +0100

    Update R Code excerpt 5 Probation and magistrates person-event data linkage.R

--- R Code excerpt 5 Probation and magistrates person-event data linkage.R
+++ R Code excerpt 5 Probation and magistrates person-event data linkage.R
@@ -9,7 +9,7 @@ library(data.table)
 library(lubridate)
 
 # Steps 1-3
-# First read in magistrates’ courts data (MAGALL), probation data (PROB) and linked criminal courts, prisons and probation dataset (LINKCJS).
+# First read in my pre-processed magistrates’ courts data (MAGALL), probation data (PROB) and linked Cross-justice system linking dataset (LINKCJS).
 # And subset variables of interest from the magistrates courts dataset, to include key ID, date and offence variables
 
 # Step 4
@@ -18,15 +18,16 @@ MAGLINK <- LINKCJS[LINKCJS$source_dataset=="mags_hocas",]
 MAGALL <-  dplyr::left_join(MAGALL, MAGLINK, by = "row_id_hash")
 
 # Step 5
-#Then using the imported variables from the linking dataset, link probation data
+#Then using the imported variables from the linking dataset to add probation linking information
 PROBLINK <- LINKCJS[LINKCJS$source_dataset=="probation_delius",] 
-MAGPROBLINK <-  dplyr::left_join(MAGALL, PROBLINK, by="estimated_mc_cc_ps_dp")  
-MAGPROBLINK <-  dplyr::distinct(MAGPROBLINK, case.id.hash, .keep_all="TRUE") # Upon linking only distinct matches were retained for linking with probation data.
+MAGPROBLINK <- dplyr::left_join(MAGALL, PROBLINK, by="estimated_xjs_id")  
+MAGPROBLINK <- dplyr::distinct(MAGPROBLINK, case_id_hash, .keep_all="TRUE") # Only distinct matches were retained for adding probation linking info
+#line 24 mitigates against the duplication that results from line 23
 PROB <-  dplyr::left_join(PROB, PROBLINK, by = "row_id_hash")
 
 # Step 6
 MAGPROBpeople <- MAGPROBLINK %>%
-  dplyr::left_join(PROB, by = "estimated_mc_cc_ps_dp") %>%
+  dplyr::left_join(PROB, by = "estimated_xjs_id") %>%
   dplyr::filter(is.na(main_offence_date) | offence_date == main_offence_date)
 
 # Step 7
@@ -40,13 +41,27 @@ MAGPROBpeople <- MAGPROBpeople %>% dplyr::filter(offence_date >= as.Date("2011-0
 
 ## Calculating reappearance measures
 
+# de-duplicate if needed
+n_distinct(MAGPROBpeople$case_id_hash)
+MAGPROBpeople <- MAGPROBpeople[!duplicated(MAGPROBpeople$case_id_hash)]
+
 #derive reappearance binary outcome
-MAGPROBpeople[, reoffended := ifelse(offence_date > referral_date[1L], 1,0)]  
+MAGPROBpeople[, reoffended := ifelse(offence_date > referral_date[1L], 1,0), by=estimated_xjs_id]  
+finaloffenders <- MAGPROBpeople[, .I[.N], by=estimated_xjs_id]$V1
+MAGPROBpeople[finaloffenders, reoffended:=0]
 
 #derive reappearance count outcome
-MAGPROBpeople[, count_reoffending := sum(offence_date > referral_date[1L], na.rm=TRUE), by=estimated_mc_cc_ps_dp]  
+MAGPROBpeople[, count_reoffending := sum(offence_date > referral_date[1L], na.rm=TRUE), by=estimated_xjs_id]  
 
 #derive duration to reappearance outcome
-data.table::setkey(MAGPROBpeople, estimated_mc_cc_ps_dp, final_result_date)
-MAGPROBpeople[, duration := lubridate::difftime(offence_date, referral_date[1L], units = "days"), by = estimated_mc_cc_ps_dp]
-MAGPROBpeople[duration <=0, duration := NA] # Nas for incidents where offence date is on or before main offence date or negative values of duration
+MAGPROBpeople[, duration := sapply(referral_date, function(x) {
+  next_dates<-offence_date[offence_date>x]
+  if(length(next_dates)>0) as.integer(min(next_dates)-x) else NA_integer_
+}), by=estimated_xjs_id]  
+
+
+# duration for EHA
+MAGPROBpeople[, durationEHA := sapply(referral_date, function(x) {
+  next_dates <- offence_date[offence_date > x]
+  if (length(next_dates) > 0) as.integer(min(min(next_dates), study_end_date) - x) else NA_integer_
+}), by = estimated_xjs_id]
